45bc27915a608b2ede8dda56a11ebd2dba934ee2
Abhinandan Singh <109.abhi@gmail.com>
2023-11-01 23:40:46 -0700

    Update template to LIF neuron

--- src/decoder.v
+++ src/decoder.v
@@ -1,43 +1,33 @@
+`default_nettype none
 
-/*
-      -- 1 --
-     |       |
-     6       2
-     |       |
-      -- 7 --
-     |       |
-     5       3
-     |       |
-      -- 4 --
-*/
-
-module seg7 (
-    input wire [3:0] counter,
-    output reg [6:0] segments
+module leaky (
+    input wire [7:0] current,
+    input wire       clk,
+    input wire       rst,
+    output wire      spike,
+    output reg [7:0] state
 );
 
-    always @(*) begin
-        case(counter)
-            //                7654321
-            0:  segments = 7'b0111111;
-            1:  segments = 7'b0000110;
-            2:  segments = 7'b1011011;
-            3:  segments = 7'b1001111;
-            4:  segments = 7'b1100110;
-            5:  segments = 7'b1101101;
-            6:  segments = 7'b1111101;
-            7:  segments = 7'b0000111;
-            8:  segments = 7'b1111111;
-            9:  segments = 7'b1101111;
-            10: segments = 7'b1110111;
-            11: segments = 7'b1111100;
-            12: segments = 7'b0111001;
-            13: segments = 7'b1011110;
-            14: segments = 7'b1111001;
-            15: segments = 7'b1110001;
-	    default:
-                segments = 7'b0000000;
-        endcase
+    reg [7:0] next_state, threshold;
+
+    always@(posedge clk)
+    begin
+        if (!rst)
+        begin
+            state <= 0;
+            threshold <= 127;
+        end
+        else
+        begin
+            state <= next_state;
+        end
+    
+        // next_state logic
+        assign next_state = current + (state >> 1);
+
+        // spiking logic
+        assign spike = (state >= threshold);
+
     end
 
 endmodule
